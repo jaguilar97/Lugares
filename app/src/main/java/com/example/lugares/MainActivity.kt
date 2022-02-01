a7f75b9db4a89f62c26861f3edb54fae6694b6dd
Johan <aguilar.garita97@gmail.com>
2022-02-01 15:21:02 -0600

    El segundo commit solo tiene un comentario adicional

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // Acá haremos algo en el video de la clase...
     }
 }
